dbc039a983b716eae7fdf06eee3580a7056d6d2f
marcwolfe <marc.wolfe21@gmail.com>
2022-01-22 16:17:08 -0600

    Home Component complete

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,11 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Home from './Home';
 
 function App() {
   return (
     <div className="App">
-      
-      
+      <Home />
     </div>
   );
 }
---
+++ portfolio/src/Home.js
@@ -0,0 +1,48 @@
+import React from 'react'
+import './Home.css'
+// import logo from './img/logo.png';
+
+function Home() {
+  return (
+    <div className='home'>
+      <div className='home__bg'>
+          <div className="header d__flex align__items__center pxy__30">
+              <div className="logo">
+                  <img src='' alt=""/>
+              </div>
+              <div className='navigation pxy__30'>
+                  <ul className="navbar d__flex">
+                        <a href="#Home"><li className="nav__items mx__15">Home</li></a>
+                        <a href="#About"><li className="nav__items mx__15">About</li></a>
+                        <a href="#Services"><li className="nav__items mx__15">Services</li></a>
+                        <a href="#Portfolio"><li className="nav__items mx__15">Portfolio</li></a>
+                        <a href="#Contact"><li className="nav__items mx__15">Contact</li></a>
+                  </ul>
+            </div>
+          </div>
+          {/* Home Content */}
+          <div className="container">
+          <div className="home__content">
+              <div className="home__meta">
+                  <h1 className="home__text pz__10">
+                      Welcome to my Portfolio
+                  </h1>
+                  <h2 className="home__text pz__10">
+                      Hello, I'm Marc Wolfe
+                  </h2>
+                  <h3 className="home__text sweet pz__10"> 
+                      Software Developer.
+                  </h3>
+                  <h4 className="home__text">
+                      based in Chicago IL
+                  </h4>
+              </div>
+             
+          </div>
+          </div>
+      </div>
+    </div>
+  )
+}
+
+export default Home
